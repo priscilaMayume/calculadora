7ec3f792a702ccc3638acb67f974d57204a7833d
Priscila <prisccila@gmail.com>
2021-02-23 00:11:21 +0000

    Implementação do while - usuário pra sair do looping deve digitar 0 para sair

--- Main.java
+++ Main.java
@@ -4,47 +4,55 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int operacao;
-        double num1;
-        double num2;
+					int operacao;
+					double num1;
+					double num2;
 
-        Scanner calc = new Scanner(System.in);
+					Scanner calc = new Scanner(System.in);
 
-        System.out.println("***************INSTRUÇÕES*****************");
+					System.out.println("***************INSTRUÇÕES*****************");
 
-        System.out.println("Digite 1 para somar.");
-        System.out.println("Digite 2 para subtrair.");
-        System.out.println("Digite 3 para multiplicar.");
-        System.out.println("Digite 4 para dividir.");
+					System.out.println("Digite 1 para somar.");
+					System.out.println("Digite 2 para subtrair.");
+					System.out.println("Digite 3 para multiplicar.");
+					System.out.println("Digite 4 para dividir.");
 
-        System.out.println("******************************************");
+					System.out.println("Digite 0 para sair.");
 
-        System.out.println("QUAL OPERAÇÃO DESEJA CALCULAR: ");
+					System.out.println("******************************************");
 
-        operacao = calc.nextInt();
+				do {
 
-        System.out.println("Digite o primeiro número: ");
-        num1 = calc.nextDouble();
+					System.out.println("QUAL OPERAÇÃO DESEJA CALCULAR: ");
+						operacao = calc.nextInt();
+						
+						if(operacao != 0) {
 
-        System.out.println("Digite o segundo número: ");
-        num2 = calc.nextDouble();
+						System.out.println("Digite o primeiro número: ");
+						num1 = calc.nextDouble();
 
-        if (operacao == 1) {
-            System.out.println ("A SOMA É IGUAL A: "+(num1 + num2));
-        } else {
-            if (operacao == 2) {
-                System.out.println("A SUBTRAÇÃO É IGUAL A: "+(num1 - num2));
-            } else {
-                if (operacao == 3) {
-                    System.out.println("A MULTIPLICAÇÃO É IGUAL A: "+(num1 * num2));
-                } else {
-                    if (operacao == 4) {
-                        System.out.println("A DIVISÃO É IGUAL A: "+(num1 / num2));
-                    } else {
-                        System.out.println("OPERAÇÃO INVALIDA - Refaça a operação do inicio");
-                    }
-                }
-            }
-        }
-    }
+						System.out.println("Digite o segundo número: ");
+						num2 = calc.nextDouble();
+
+						if (operacao == 1) {
+								System.out.println ("A SOMA É IGUAL A: "+(num1 + num2));
+						} else {
+								if (operacao == 2) {
+										System.out.println("A SUBTRAÇÃO É IGUAL A: "+(num1 - num2));
+								} else {
+										if (operacao == 3) {
+												System.out.println("A MULTIPLICAÇÃO É IGUAL A: "+(num1 * num2));
+										} else {
+												if (operacao == 4) {
+														System.out.println("A DIVISÃO É IGUAL A: "+(num1 / num2));
+														}
+									System.out.println("******************************************");
+										System.out.println("******************************************");
+												}
+										}
+								}
+						}
+				} while(operacao!=0);
+		}
+		
 }
